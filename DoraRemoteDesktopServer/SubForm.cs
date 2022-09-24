06c3a2e849794a54eed079b15b039ee9b4c2d446
Dorashiro <dorasiro1@live.jp>
2022-09-24 12:51:01 +0900

    [Commit]Aleaは使用できないことが分かったのでテスト用ボタンを削除しCUDA用のボタンを追加

--- DoraRemoteDesktopServer/SubForm.cs
+++ DoraRemoteDesktopServer/SubForm.cs
@@ -763,9 +763,9 @@ namespace DoraRemoteDesktopServer
             timer.Start();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
